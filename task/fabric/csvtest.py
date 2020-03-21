56cd9220cbd1d6f70d14ec4d053efe72fed08d44
zhangfeng79212@163.com <zhangfeng79212@163.com>
2020-03-21 15:49:10 +0800

    999

--- task/fabric/csvtest.py
+++ task/fabric/csvtest.py
@@ -3,16 +3,25 @@ import os
 import codecs
 import fabric
 
-date=20200308
-ip='47.93.23.227'
-password='ZQ7h5@Bt#Ct8'
-f=codecs.open("test.csv", "w",'utf-8')
+date=20200316
+
+f=codecs.open("test"+str(date)+".csv", "w",'utf-8')
 writer=csv.writer(f)
 writer.writerow([u'机器名字',date])
-def getnumber(ip):
+def getnumber(ip,hosttype):
     try:
-        conn = fabric.Connection(ip , user = 'root',port = 22, config = None, connect_kwargs={"password": password})
-        result=conn.run('ls '+str(date)+'|wc -l')
+        if hosttype=='ms':
+            conn = fabric.Connection(ip, user='ms', port=22, config=None, connect_kwargs={"password": 'cintel1234!@#$'})
+            result=conn.run('ls /home/ms/cin/voxdst/media/800002/ccrecord/'+str(date)+'|wc -l')
+        if hosttype=='media':
+            conn = fabric.Connection(ip, user='media', port=22, config=None, connect_kwargs={"password": 'cintel123'})
+            result = conn.run('ls /home/media/media/800002/ccrecord/' + str(date) + '|wc -l')
+        if hosttype== 'media1':
+            conn = fabric.Connection(ip, user='media', port=22, config=None, connect_kwargs={"password": '123456'})
+            result = conn.run('ls /home/media/media/666666/ccrecord/' + str(date) + '|wc -l')
+        if hosttype=='ms1':
+            conn = fabric.Connection(ip, user='ms', port=22, config=None, connect_kwargs={"password": 'cintel1234!@#$'})
+            result=conn.run('ls /home/ms/cin/voxdst/media/666666/ccrecord/'+str(date)+'|wc -l')
         number=result.stdout.strip()
         if not number.isdigit():
             number=0
@@ -23,16 +32,54 @@ def getnumber(ip):
         number=0
     finally:
         conn.close()
-        return 0
-writer.writerow(['ms1xxxx',getnumber(ip)])
-writer.writerow(['ms2xxxx','456'])
-writer.writerow(['ms3xxxx','456'])
-writer.writerow(['ms4xxxx','456'])
-writer.writerow(['ms5xxxx','456'])
-writer.writerow(['ms6xxxx','456'])
-writer.writerow(['ms7xxxx','456'])
-writer.writerow(['ms8xxxx','456'])
-writer.writerow(['mstotal','456'])
-writer.writerow(['media91','456'])
-writer.writerow(['media92','456'])
+        return number
+ms1n=getnumber('10.30.2.101','ms')
+writer.writerow(['ms1node2',ms1n])
+ms2n=getnumber('10.30.2.102','ms')
+writer.writerow(['ms2node2',ms2n])
+ms3n=getnumber('10.30.2.103','ms')
+writer.writerow(['ms3node2',ms3n])
+ms4n=getnumber('10.30.2.104','ms')
+writer.writerow(['ms4node2',ms4n])
+ms5n=getnumber('10.30.2.105','ms')
+writer.writerow(['ms5node2',ms5n])
+ms6n=getnumber('10.30.2.106','ms')
+writer.writerow(['ms6node2',ms6n])
+ms7n=getnumber('10.30.2.107','ms')
+writer.writerow(['ms7node2',ms7n])
+ms8n=getnumber('10.30.2.108','ms')
+writer.writerow(['ms8node2',ms8n])
+msxn=int(ms1n)+int(ms2n)+int(ms3n)+int(ms4n)+int(ms5n)+int(ms6n)+int(ms7n)+int(ms8n)
+writer.writerow(['mstotal',msxn])
+media91n=getnumber('10.30.2.91','media')
+writer.writerow(['media91',media91n])
+media92n=getnumber('10.30.2.92','media')
+writer.writerow(['media92',media92n])
+
+##node3
+ms1n=getnumber('10.30.3.101','ms')
+writer.writerow(['ms1node3',ms1n])
+ms2n=getnumber('10.30.3.102','ms')
+writer.writerow(['ms2node3',ms2n])
+ms3n=getnumber('10.30.3.103','ms')
+writer.writerow(['ms3node3',ms3n])
+ms4n=getnumber('10.30.3.104','ms')
+writer.writerow(['ms4node3',ms4n])
+ms5n=getnumber('10.30.3.105','ms')
+writer.writerow(['ms5node3',ms5n])
+ms6n=getnumber('10.30.3.106','ms')
+writer.writerow(['ms6node3',ms6n])
+ms7n=getnumber('10.30.3.107','ms')
+writer.writerow(['ms7node3',ms7n])
+ms8n=getnumber('10.30.3.108','ms')
+writer.writerow(['ms8node3',ms8n])
+msxn=int(ms1n)+int(ms2n)+int(ms3n)+int(ms4n)+int(ms5n)+int(ms6n)+int(ms7n)+int(ms8n)
+writer.writerow(['mstotal',msxn])
+media91n=getnumber('10.30.3.91','media')
+writer.writerow(['media91',media91n])
+media92n=getnumber('10.30.3.92','media')
+writer.writerow(['media92',media92n])
+
+#node1
+
 f.close()
