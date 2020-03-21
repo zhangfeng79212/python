036ea5476495a543eeb94b508349db9db15df7dc
zhangfeng79212@163.com <zhangfeng79212@163.com>
2020-03-21 09:56:19 +0800

    12

--- task/fabric/csvtest.py
+++ task/fabric/csvtest.py
@@ -1,12 +1,30 @@
 import csv
 import os
 import codecs
-date=20200308
+import fabric
 
+date=20200308
+ip='47.93.23.227'
+password='ZQ7h5@Bt#Ct8'
 f=codecs.open("test.csv", "w",'utf-8')
 writer=csv.writer(f)
 writer.writerow([u'机器名字',date])
-writer.writerow(['ms1xxxx','456'])
+def getnumber(ip):
+    try:
+        conn = fabric.Connection(ip , user = 'root',port = 22, config = None, connect_kwargs={"password": password})
+        result=conn.run('ls '+str(date)+'|wc -l')
+        number=result.stdout.strip()
+        if not number.isdigit():
+            number=0
+        return number
+    except Exception as ex:
+        print("execute fail!!")
+        print(ex)
+        number=0
+    finally:
+        conn.close()
+        return 0
+writer.writerow(['ms1xxxx',getnumber(ip)])
 writer.writerow(['ms2xxxx','456'])
 writer.writerow(['ms3xxxx','456'])
 writer.writerow(['ms4xxxx','456'])
