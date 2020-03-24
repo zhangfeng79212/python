30ce5d708ae69a4dd6a8bb0f87bc89d28fa8913b
zhangfeng79212@163.com <zhangfeng79212@163.com>
2020-03-24 21:00:12 +0800

    12

--- interface/mp3.py
+++ interface/mp3.py
@@ -7,6 +7,7 @@ ip='47.93.23.227'
 url1='http://'+ip+':14502'
 #获取srfmsgid和ms
 header={'Content-Type': 'text/plain;charset=UTF-8'}
+header2={'Content-Type':'application/download'}
 data = json.dumps({
 "request":"get_cti_callrecord_callid",
 "total": "",
@@ -22,8 +23,14 @@ r = requests.post("http://47.93.23.227:14502/recordService", headers=header,data
 print(r.text)
 print(r.json().get('output').get('get_cti_callrecord_callid'))
 for k in r.json().get('output').get('get_cti_callrecord_callid'):
-    print(k.get('srfmsgid'))
-    print(k.get('msserver'))
+    str1=k.get('srfmsgid')
+    str2=k.get('msserver')
+    if(str1 and str2 ):
+        r1=requests.post("http://47.93.23.227:8068/cincc-serv/media/download?msServer=media&filepath="+str1,\
+                 headers=header2)
+        print(str1)
+        print(r1)
 
 
+#http://10.30.1.153:8068/cincc-serv/media/download?msServer=ms15&filePath=666666/servicerecord/000001666666006004/20200323/20005A18826438981C202003232226560A1E011626002945S202003232227060624920A1E011603005393.mp3
 #获取录音文件
